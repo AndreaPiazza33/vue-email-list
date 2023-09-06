11b937f4bf657b816c1b08faa142a2b379f34915
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-09-06 17:20:53 +0200

    feat: add cycle to get other emails

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,31 @@
-//recupero email
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then((email) => {
-console.log(email.data.response); 
-}); 
+
+// VUE
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+       //contenitore email randomiche 
+       randomEmail:[] 
+    };
+  },
+  methods: {
+    //funzione per recuperare tot(max) email
+    getMails(max){
+        for (let index = 0; index < max; index++) {
+        //recupero email con axios(azione che si deve ripetere per tot volte)
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((email) => {
+                const getEmail = email.data.response;
+                this.randomEmail.push(getEmail) 
+            });       
+        };
+    },
+  },
+  mounted() {
+    //numero di email da recuperare (dopo il caricamento)
+    this.getMails(10);
+  },
+}).mount('#app');
 
 
